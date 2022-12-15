1e15b172469b24fb5bda8dc8d4a1c239104fe9e1
Ana Bennett <anabennett1111@gmail.com>
2022-12-15 10:52:25 -0600

    primeNumber

--- primeNumber.js
+++ primeNumber.js
@@ -1,1 +1,19 @@
-//Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+//Define a function that takes an integer argument and returns 
+//a logical value true or false depending on if the integer is a prime.
+
+function isPrime(num) {
+  if(num < 2) {
+    return false
+  }
+  if(num === 2) {
+    return true
+  }
+
+  const maximumDividor = Math.sqrt(num) + 1
+  for (var i = 2; i < maximumDividor; i++) {
+    if(num % i === 0) {
+      return false
+    }
+  }
+  return true;
+}
